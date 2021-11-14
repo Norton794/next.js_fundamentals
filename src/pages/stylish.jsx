74b17666fa00cb87acfe08f5fd08c9c14d0cd438
Norton <nortonsantos79@gmail.com>
2021-11-14 21:40:41 -0200

    using modularized css

---
+++ src/pages/stylish.jsx
@@ -0,0 +1,11 @@
+import styles from "../styles/Stylish.module.css"
+
+export default function Stylish() {
+    return (
+        <div className={styles.purple}>
+            <h1>Stylizing using css modules</h1>
+        </div>
+    );
+
+
+}
