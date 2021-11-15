c2b2b7bd24e8e619386ae7569f117e2c978dab21
Norton <nortonsantos79@gmail.com>
2021-11-15 00:37:17 -0200

    example.jsx

---
+++ src/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+    return (
+        <header>
+            <h1>Fundamentals of Next.js & React</h1>
+        </header>
+    )
+}
---
+++ src/pages/example.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/Header";
+
+export default function Example() {
+    return (
+        <div>
+            <Header />
+            <Header />
+        </div>
+
+    );
+}
