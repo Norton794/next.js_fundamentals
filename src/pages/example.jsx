88fad7afdd67755a616c4b52583a49219832545d
Norton <nortonsantos79@gmail.com>
2021-11-15 00:39:42 -0200

    using fragments

--- src/pages/example.jsx
+++ src/pages/example.jsx
@@ -2,10 +2,10 @@ import Header from "../components/Header";
 
 export default function Example() {
     return (
-        <div>
+        <>
             <Header />
             <Header />
-        </div>
+        </>
 
     );
 }
